3646d787589f6c63a822c3208c15898c42f141af
DPalaniappan <123602546+DPalaniappan@users.noreply.github.com>
2025-10-14 21:12:53 -0400

    Added FirstPerson Camera Controls

---
+++ Assets/Scripts/Player Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 10000f;
+    public Transform playerBody; 
+    private float xRotation = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Locking the cursor to the center of the screen
+        Cursor.visible = false; //Hiding the cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Getting input from mouse for X and Y axis
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); //Clamping the rotation so we don't flip over
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        //Vecotr3.up is the Y axis that we rotate upon
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
