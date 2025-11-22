ab969cc8aef3578a80c45566d29b4fad5523f075
Nathan Loney <njloney@gmail.com>
2025-11-21 20:10:07 -0500

    fixing merge stuff

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: CMSC425-Game
 - Unity version: Unity 6000.0.59f2
 - Active game object:
-  - Name: Wand
+  - Name: WandModel
   - Tag: Untagged
   - Layer: Enemy
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/ObjectScripts/CrystalType.cs
+++ Assets/Scripts/ObjectScripts/CrystalType.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public enum CrystalType
 {
-<<<<<<< HEAD
-    Fire, Ice, Wind, Light, Dark, Earth, Lightning
-=======
     Fire,
     Ice,
     Wind,
     Earth,
     Dark,
+    Light,
     Lightning,
     None
 
 
->>>>>>> new_player_stuff
 }
